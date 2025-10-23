e08204540f5ce4ab63d60799800c9b363308dd5f
[dyad] <valtim_junim@hotmail.com>
2025-10-23 00:50:34 -0300

    [dyad] wrote 1 file(s)

--- src/components/AnnualScheduleCalendar.tsx
+++ src/components/AnnualScheduleCalendar.tsx
@@ -159,11 +159,10 @@ const AnnualScheduleCalendar: React.FC<AnnualScheduleCalendarProps> = ({ initial
                 <TooltipTrigger asChild>
                   <div
                     className={cn(
-                      "h-6 w-6 rounded-full text-xs font-medium text-white cursor-default transition-colors duration-100 text-center", // Removido flex/absolute, adicionado text-center
+                      "h-6 w-6 rounded-full text-xs font-medium text-white cursor-default transition-colors duration-100 grid place-items-center", // Usando grid place-items-center para centralização robusta
                       colorClass,
                       isCurrentDay && 'ring-2 ring-offset-1 ring-primary dark:ring-primary',
                     )}
-                    style={{ lineHeight: '24px' }} // Força line-height igual à altura (h-6 = 24px)
                   >
                     {dayOfMonth}
                   </div>
