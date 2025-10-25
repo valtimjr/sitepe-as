aeedecea0806ff2baa761f36afa3e066f9dc8887
[dyad] <valtim_junim@hotmail.com>
2025-10-25 01:38:16 -0300

    [dyad] wrote 2 file(s)

--- src/components/PartListItemForm.tsx
+++ src/components/PartListItemForm.tsx
@@ -1,5 +1,4 @@
 "use client";
-/** @jsxImportSource react */
 import React, { useState, useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
