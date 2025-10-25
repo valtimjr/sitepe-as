7806d471550db7525a1fc3e4da959316927afc48
[dyad] <valtim_junim@hotmail.com>
2025-10-25 18:56:47 -0300

    [dyad] wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -27,32 +27,46 @@ const queryClient = new QueryClient();
 
 // Componente Wrapper para usar o hook
 const AppWrapper = () => {
-  useOfflineSync();
-  
+  // O hook useOfflineSync deve ser chamado dentro do SessionContextProvider
+  // Vamos movê-lo para o componente SessionContextProvider ou para um componente filho.
+  // Para simplificar, vamos envolver o AppWrapper com BrowserRouter e SessionContextProvider
+  // e mover a chamada do hook para um componente que encapsula as rotas.
+
   return (
     <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <SessionContextProvider>
-        <AppHeader />
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/search-parts" element={<SearchParts />} />
-          <Route path="/parts-list" element={<PartsList />} />
-          <Route path="/service-orders" element={<ServiceOrderList />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/admin" element={<DatabaseManagerPage />} />
-          <Route path="/signup/:uuid" element={<SignupPage />} />
-          <Route path="/reset-password" element={<ResetPasswordPage />} />
-          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-          <Route path="/settings" element={<UserSettingsPage />} />
-          <Route path="/time-tracking" element={<TimeTrackingPage />} />
-          <Route path="/custom-list/:listId" element={<CustomListPage />} />
-          <Route path="/my-custom-lists" element={<MyCustomListsPage />} />
-          <Route path="/custom-menu-view" element={<CustomMenuOverview />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <AppContent />
       </SessionContextProvider>
     </BrowserRouter>
+  );
+};
+
+// Novo componente para conter o hook e as rotas
+const AppContent = () => {
+  useOfflineSync();
+  
+  return (
+    <>
+      <AppHeader />
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/search-parts" element={<SearchParts />} />
+        <Route path="/parts-list" element={<PartsList />} />
+        <Route path="/service-orders" element={<ServiceOrderList />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<DatabaseManagerPage />} />
+        <Route path="/signup/:uuid" element={<SignupPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/settings" element={<UserSettingsPage />} />
+        <Route path="/time-tracking" element={<TimeTrackingPage />} />
+        <Route path="/custom-list/:listId" element={<CustomListPage />} />
+        <Route path="/my-custom-lists" element={<MyCustomListsPage />} />
+        <Route path="/custom-menu-view" element={<CustomMenuOverview />} />
+        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 };
 
