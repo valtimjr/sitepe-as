d8c4a99f2971e09300d9405c62c3cc1339228be0
[dyad] <valtim_junim@hotmail.com>
2025-11-04 19:54:57 -0300

    [dyad] wrote 1 file(s)

--- src/components/CustomListEditor.tsx
+++ src/components/CustomListEditor.tsx
@@ -29,6 +29,17 @@ import {
 } from '@/components/ui/table';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'; // Importar Popover
 import PartSearchInput from './PartSearchInput'; // IMPORT CORRIGIDO
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"; // ADICIONADO
 
 interface CustomListEditorProps {
   list: CustomList;
