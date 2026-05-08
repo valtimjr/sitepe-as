3d381258cea1ed41469f5a13b8d639d47f168143
[dyad] <valtim_junim@hotmail.com>
2026-05-07 23:52:07 -0300

    [dyad] Corrigindo importações e criando páginas faltantes - wrote 3 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,25 @@ import { SessionContextProvider } from './components/SessionContextProvider';
 import { CompanyProvider } from './context/CompanyContext';
 import LandingPage from './pages/LandingPage';
 import Index from './pages/Index';
-import ServiceOrderPage from './pages/ServiceOrderPage';
+import ServiceOrderList from './pages/ServiceOrderList';
 import TimeTrackingPage from './pages/TimeTrackingPage';
-import MonthlySummaryPage from './pages/MonthlySummaryPage';
 import UserSettingsPage from './pages/UserSettingsPage';
 import AdminReportPage from './pages/AdminReportPage';
 import MenuManagerPage from './pages/MenuManagerPage';
 import AdminConfigPage from './pages/AdminConfigPage';
-import CustomMenuView from './pages/CustomMenuView';
 import TagManagerPage from './pages/TagManagerPage';
+import DatabaseManagerPage from './pages/DatabaseManagerPage';
 import LoginPage from './pages/Login';
-import InviteValidator from './pages/InviteValidator';
+import SignupPage from './pages/SignupPage';
+import PartsList from './pages/PartsList';
+import SearchParts from './pages/SearchParts';
+import CustomListPage from './pages/CustomListPage';
+import CustomMenuOverview from './pages/CustomMenuOverview';
+import MyCustomListsPage from './pages/MyCustomListsPage';
+import ResetPasswordPage from './pages/ResetPasswordPage';
+import ForgotPasswordPage from './pages/ForgotPasswordPage';
+import CookiePolicyPage from './pages/CookiePolicyPage';
+import NotFound from './pages/NotFound';
 import ToasterProvider from './components/ToastProvider';
 
 function App() {
@@ -28,26 +36,32 @@ function App() {
             {/* Landing & Auth */}
             <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/validate-invite" element={<InviteValidator />} />
+            <Route path="/signup/:uuid" element={<SignupPage />} />
+            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+            <Route path="/reset-password" element={<ResetPasswordPage />} />
+            <Route path="/cookie-policy" element={<CookiePolicyPage />} />
             
             {/* Company Routes */}
             <Route path="/:company" element={<Index />} />
-            <Route path="/:company/service-orders" element={<ServiceOrderPage />} />
+            <Route path="/:company/service-orders" element={<ServiceOrderList />} />
             <Route path="/:company/time-tracking" element={<TimeTrackingPage />} />
-            <Route path="/:company/monthly-summary" element={<MonthlySummaryPage />} />
             <Route path="/:company/settings" element={<UserSettingsPage />} />
+            <Route path="/:company/parts-list" element={<PartsList />} />
+            <Route path="/:company/search-parts" element={<SearchParts />} />
+            <Route path="/:company/custom-menu-view" element={<CustomMenuOverview />} />
+            <Route path="/:company/custom-list/:listId" element={<CustomListPage />} />
+            <Route path="/:company/my-custom-lists" element={<MyCustomListsPage />} />
             
             {/* Admin Routes */}
+            <Route path="/:company/admin" element={<DatabaseManagerPage />} />
             <Route path="/:company/admin-report" element={<AdminReportPage />} />
             <Route path="/:company/menu-manager" element={<MenuManagerPage />} />
             <Route path="/:company/admin-config" element={<AdminConfigPage />} />
             <Route path="/:company/manage-tags" element={<TagManagerPage />} />
             
-            {/* Dynamic Custom Menu */}
-            <Route path="/:company/menu/:menuId" element={<CustomMenuView />} />
-            
             {/* Fallback */}
-            <Route path="*" element={<Navigate to="/" replace />} />
+            <Route path="/404" element={<NotFound />} />
+            <Route path="*" element={<Navigate to="/404" replace />} />
           </Routes>
           <ToasterProvider />
         </Router>
