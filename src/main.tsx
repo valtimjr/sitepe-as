d5404fad1dff35aa1bbcdd1ef48dce1a0f00dc18
[dyad] <valtim_junim@hotmail.com>
2026-07-09 00:10:58 -0300

    [dyad] wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./globals.css";
+if (typeof global === 'undefined') {
+  (window as any).global = window;
+}
 
-createRoot(document.getElementById("root")!).render(<App />);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
