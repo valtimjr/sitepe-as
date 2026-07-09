4bdfbc4756560bb488df95630591a54698e1041f
[dyad] <valtim_junim@hotmail.com>
2026-07-09 00:15:37 -0300

    Reverted all changes back to version a1968783da9ae2c3a6ca2595fe44dc8b36ac6a0e

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,5 @@
-if (typeof global === 'undefined') {
-  (window as any).global = window;
-}
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./globals.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);
